f20c3faa3346827b1345e9e48bac17c16a5a75d9
refdd <titorefat76@example.com>
2022-12-20 16:45:51 +0200

    form octsy

---
+++ forminfo/formInfo.js
@@ -0,0 +1,23 @@
+new TomSelect("#select-beast",{
+	create: true,
+	sortField: {
+		field: "text",
+		direction: "asc"
+	}
+});
+new TomSelect("#select-code",{
+	create: true,
+    itemClass: 'refat',
+	sortField: {
+		field: "text",
+		direction: "asc"
+	}
+});
+new TomSelect("#select-budget",{
+	create: true,
+    itemClass: 'refat',
+	sortField: {
+		field: "text",
+		direction: "asc"
+	}
+});
