0ffb36ee059ae315480253a898e77a350a3fa433
refdd <titorefat76@example.com>
2022-12-25 15:24:12 +0200

    from refat puah last push

--- forminfo/formInfo.js
+++ forminfo/formInfo.js
@@ -21,3 +21,62 @@ new TomSelect("#select-budget",{
 		direction: "asc"
 	}
 });
+
+
+const children = document.getElementById("Children");
+const aduilt = document.getElementById("aduilt");
+const Infants = document.getElementById("Infants");
+const addButton = document.querySelectorAll(".add")  
+const removeButton = document.querySelectorAll(".remove")  
+let childrenText = 0;
+let aduiltText = 0;
+let InfantsText = 0;
+
+children.innerHTML ="children"
+aduilt.innerHTML ="aduilt"
+Infants.innerHTML ="Infants"
+
+// event conter 
+// add
+function addCounter(type) {
+	if(type == "aduilt"){
+		aduilt.innerHTML = aduiltText += 1
+	}
+	if(type == "Children"){
+		children.innerHTML = childrenText += 1
+	}
+	if(type == "infants"){
+		Infants.innerHTML = InfantsText += 1
+	}
+	
+}
+
+
+// reomve
+function removeCounter (type){
+
+	if(type == "aduilt"  ){
+	if(aduilt.innerHTML <= 1 ){
+		aduilt.innerHTML = "aduilt"
+	}else{
+
+		aduilt.innerHTML = aduiltText -= 1
+	}
+	}
+	if(type == "Children"){
+		if(children.innerHTML <= 1 ){
+			children.innerHTML = "Children"
+		}else{
+	
+			children.innerHTML = childrenText -= 1
+		}
+	}
+	if(type == "infants"){
+		if(Infants.innerHTML <= 1 ){
+			Infants.innerHTML = "infants"
+		}else{
+	
+			Infants.innerHTML = InfantsText -= 1
+		}
+	}
+}
--- main.js
+++ main.js
@@ -1,15 +1,97 @@
+// // selector 
+// const card = document.querySelector(".card");
+// const nextButton = document.querySelector(".next_button");
+
+// console.log(inputChek)
+
+
+// // evnts
+// card && card.addEventListener("click", ()=>{
+//     card.classList.toggle("active")
+//     if (card.classList.contains("active")  ){
+//         nextButton.classList.add("active");
+//         nextButton.classList.add("twark");
+//         nextButton.innerHTML="Next Step";
+//       } else {
+//         nextButton.classList.remove("active");
+//         nextButton.classList.remove("twark");
+//         nextButton.innerHTML="Select Your Destination";
+//       }
+//     if(inputChek.checked == false) {
+//        inputChek.checked = true; 
+//     }
+//     else {
+//         if(inputChek.checked == true) {
+//            inputChek.checked = false; 
+//          }   
+//     }
+// })
+// // evnts
+
+// inputChek.addEventListener('change', function() {
+//     if (this.checked) {
+//         card.classList.add("active");
+//         nextButton.classList.add("active");
+//         nextButton.classList.add("twark");
+//         nextButton.innerHTML="Next Step";
+//     } else {
+//         card.classList.remove("active")
+//         nextButton.classList.remove("active");
+//         nextButton.classList.remove("twark");
+//         nextButton.innerHTML="Select Your Destination";
+//     }
+//   });
+// //   if button active 
+// nextButton && nextButton.addEventListener('click', ()=>{
+//     if(nextButton.classList.contains("active")){
+//         window.open("./calendar/calendar.html");
+//         }else{
+//           alert("Please select")
+//         }
+// })
 // selector 
-const card = document.querySelector(".card");
-const inputChek = document.querySelector(".checkbox-animate label input");
+const card = document.querySelectorAll(".card");
+const inputChek = document.querySelectorAll(".checkbox-animate label input");
 const nextButton = document.querySelector(".next_button");
-
-console.log(inputChek)
-
-
-// evnts
-card && card.addEventListener("click", ()=>{
-    card.classList.toggle("active")
-    if (card.classList.contains("active")  ){
+const selectedText = document.querySelectorAll(".cardSlected");
+const counntryText = document.querySelectorAll(".cardContent");
+  
+console.log('====================================');
+console.log(selectedText , counntryText) ; 
+console.log('====================================');
+// function  add chack box 
+const checkbox = (index)=>{
+ if(inputChek[index].checked == false) {
+  inputChek[index].checked = true; 
+            }
+            else {
+                if(inputChek[index].checked == true) {
+                  inputChek[index].checked = false; 
+                 }   
+            }
+}
+// ---------
+card && card.forEach((item , index )=>{
+  item.addEventListener("click", ()=>{
+    item.classList.toggle("active")
+    counntryText[index].classList.toggle("active")
+    selectedText[index].classList.toggle("active")
+      checkbox(index)
+      refatbox(index) 
+   
+  })
+})
+// function select check box 
+let arele = []
+const refatbox = (index)=>{
+// arele.push(index)
+if(arele.includes(index)){
+  const reomveall =  arele.filter(item => item !== index)
+  arele = [...reomveall]
+}else{
+  arele.push(index)
+}
+if (arele.length !== 0  ){
         nextButton.classList.add("active");
         nextButton.classList.add("twark");
         nextButton.innerHTML="Next Step";
@@ -18,35 +100,23 @@ card && card.addEventListener("click", ()=>{
         nextButton.classList.remove("twark");
         nextButton.innerHTML="Select Your Destination";
       }
-    if(inputChek.checked == false) {
-       inputChek.checked = true; 
-    }
-    else {
-        if(inputChek.checked == true) {
-           inputChek.checked = false; 
-         }   
-    }
-})
-// evnts
-
-inputChek.addEventListener('change', function() {
-    if (this.checked) {
-        card.classList.add("active");
-        nextButton.classList.add("active");
-        nextButton.classList.add("twark");
-        nextButton.innerHTML="Next Step";
-    } else {
-        card.classList.remove("active")
-        nextButton.classList.remove("active");
-        nextButton.classList.remove("twark");
-        nextButton.innerHTML="Select Your Destination";
-    }
-  });
-//   if button active 
+      //   if button active 
 nextButton && nextButton.addEventListener('click', ()=>{
-    if(nextButton.classList.contains("active")){
-        window.open("./calendar/calendar.html");
-        }else{
-          alert("Please select")
-        }
+  if(arele.length !== 0 ){
+      window.open("./calendar/calendar.html");
+      }else{
+        alert("Please select")
+      }
+})
+ 
+}
+// function chack box 
+inputChek && inputChek.forEach((item , index )=>{
+  item.addEventListener("change", (e)=>{
+    if (e.target.checked) {
+              card[index].classList.add("active");
+          } else {
+              card[index].classList.remove("active")
+          }
+  })
 })
